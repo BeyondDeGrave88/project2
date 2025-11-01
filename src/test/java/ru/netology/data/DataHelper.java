4e9d87dd507bf465c977853f84f6eecfc82c54d9
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-11-01 16:36:44 +0300

    DataHelper changed

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -3,25 +3,18 @@ package ru.netology.data;
 import com.github.javafaker.Faker;
 import lombok.Value;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class DataHelper {
-    private static final Faker FAKER = new Faker();
+    private static final Faker FAKER = new Faker(new Locale("en"));
+    private static final Faker FAKER_RU = new Faker(new Locale("ru"));
 
     private DataHelper() {
     }
-        @Value
-        public static class ApprovedCardInfo {
-            String cardNumber;
-            String month;
-            String year;
-            String owner;
-            String cvc;
-        }
 
-    public static ApprovedCardInfo getApprovedCardInfo() {
-        return new ApprovedCardInfo("1111_2222_3333_4444", "12", "26", "IVAN_IVANOV", "123");
-    }
     @Value
-    public static class DeclinedCardInfo {
+    public static class CardInfo {
         String cardNumber;
         String month;
         String year;
@@ -29,7 +22,118 @@ public class DataHelper {
         String cvc;
     }
 
-    public static DeclinedCardInfo getDeclinedCardInfo() {
-        return new DeclinedCardInfo("5555_6666_7777_8888", "12", "26", "IVAN_IVANOV", "123");
+    public static CardInfo getApprovedCardInfo() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
     }
+
+    public static CardInfo getDeclinedCardInfo() {
+        return new CardInfo(getValidDeclinedCard(), getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
+    }
+    public static String getValidApprovedCard() {
+        return "1111 2222 3333 4444";
+    }
+    public static String getValidDeclinedCard() {
+        return "5555 6666 7777 8888";
+    }
+
+    public static String getValidMonth() {
+        return String.format("%02d", LocalDate.now().getMonthValue());
+    }
+
+    public static String getValidYear() {
+        return String.format("%02d", LocalDate.now().getYear() % 100 + 1);
+    }
+
+    public static String getValidOwner() {
+        return FAKER.name().firstName().toUpperCase() + " " + FAKER.name().lastName().toUpperCase();
+    }
+
+    public static String getValidCvc() {
+        return String.format("%03d", FAKER.number().numberBetween(1, 999));
+    }
+
+    //               Методы для негативных тестов
+    public static CardInfo getNonExistentCardNumber() {
+        return new CardInfo("0000 0000 0000 0000",getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
+    }
+
+    public static CardInfo getInvalidCardWithShortNumber() {
+        return new CardInfo("1111 2222 3333",getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
+    }
+
+    public static CardInfo getCardNumberWithInvalidMonth() {
+        return new CardInfo(getValidApprovedCard(),getInvalidMonth(),getValidYear(),getValidOwner(),getValidCvc());
+    }
+
+    public static String getInvalidMonth() {
+        return String.valueOf(FAKER.number().numberBetween(13, 100));
+    }
+
+    public static CardInfo getCardNumberWithExpiredMonth() {
+        return new CardInfo(getValidApprovedCard(),getExpiredMonth(),getCurrentYear(),getValidOwner(),getValidCvc());
+    }
+
+    public static String getExpiredMonth() {
+        int currentMonth = LocalDate.now().getMonthValue();
+        int expiredMonth = currentMonth - 1;
+        if (expiredMonth <= 0) expiredMonth = 12;
+        return String.format("%02d", expiredMonth);
+    }
+
+    public static CardInfo getCardNumberWithExpiredYear() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getExpiredYear(), getValidOwner(), getValidCvc());
+    }
+
+    public static String getCurrentYear() {
+        int currentYear = LocalDate.now().getYear() % 100;
+        return String.format("%02d", currentYear);
+    }
+
+    public static String getExpiredYear() {
+        int currentYear = LocalDate.now().getYear() % 100;
+        return String.format("%02d", currentYear - 1);
+    }
+
+    public static CardInfo getCardNumberWithCyrillicOwner() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), getInvalidOwnerCyrillic(), getValidCvc());
+    }
+    public static String getInvalidOwnerCyrillic() {
+        return FAKER_RU.name().firstName().toUpperCase() + " " + FAKER_RU.name().lastName().toUpperCase();
+    }
+
+    public static CardInfo getCardNumberWithShortCvc() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), getValidOwner(), getInvalidCvcShort());
+    }
+
+    public static String getInvalidCvcShort() {
+        return String.format("%02d", FAKER.number().numberBetween(0, 100));
+    }
+    public static CardInfo getCardNumberWithLongCvc() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), getValidOwner(), getInvalidCvcLong());
+    }
+
+    public static String getInvalidCvcLong() {
+        return String.format("%04d", FAKER.number().numberBetween(0, 10000));
+    }
+
+    public static CardInfo getCardWithEmptyNumber() {
+        return new CardInfo("", getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
+    }
+
+    public static CardInfo getCardWithEmptyMonth() {
+        return new CardInfo(getValidApprovedCard(), "", getValidYear(), getValidOwner(), getValidCvc());
+    }
+
+    public static CardInfo getCardWithEmptyYear() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), "", getValidOwner(), getValidCvc());
+    }
+
+    public static CardInfo getCardWithEmptyOwner() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), "", getValidCvc());
+    }
+
+    public static CardInfo getCardWithEmptyCvc() {
+        return new CardInfo(getValidApprovedCard(), getValidMonth(), getValidYear(), getValidOwner(), "");
+    }
+
 }
