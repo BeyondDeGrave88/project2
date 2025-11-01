3552b67008a67a6fb87df6bf7873c030d9d192b1
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-11-01 16:37:41 +0300

    DashboardPage changed

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,4 +1,26 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
 public class DashboardPage {
+
+    private SelenideElement header = $(Selectors.byText("Путешествие дня"));
+    private SelenideElement buyButton = $(Selectors.byText("Купить"));
+    private SelenideElement creditButton = $(Selectors.byText("Купить в кредит"));
+
+    public DashboardPage() {
+        header.shouldBe(Condition.visible);
+    }
+    public PayByCardPage goToPayByCard() {
+        buyButton.click();
+        return new PayByCardPage();
+    }
+
+    public PayByCardPage goToCreditByCard() {
+        creditButton.click();
+        return new PayByCardPage();
+    }
 }
